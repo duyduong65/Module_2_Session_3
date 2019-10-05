76171bbfdef6dcb599b80c11d9050ea7514f3a8b
duyduong65 <tg.bluesky66@gmail.com>
2019-10-06 01:26:19 +0700

    add class fan

--- class_Fan/class_Fan.php
+++ class_Fan/class_Fan.php
@@ -1,1 +1,28 @@
 <?php
+
+class Fan
+{
+    const low = 1;
+    const medium = 2;
+    const fast = 3;
+    private $speed;
+    private $on;
+    private $radius;
+    private $color;
+
+    function __construct($speed = self::low, $on = false, $radius = 5, $color = "blue")
+    {
+        $this->speed = $speed;
+        $this->on = $on;
+        $this->radius = $radius;
+        $this->color = $color;
+    }
+
+    function getSpeed(){
+        return $this->speed;
+    }
+    function setSpeed (){
+
+    }
+
+}
